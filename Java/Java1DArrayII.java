842ccdfaa0286341253bd095983312da08d6e649
Om Wagh <omwaghwaghpatta@gmail.com>
2023-04-03 20:51:08 +0530

    03/04/2023

--- Java/Java1DArrayII.java
+++ Java/Java1DArrayII.java
@@ -6,10 +6,10 @@
 
 import java.util.*;
 
-public class Java1DArrayII {
-
-    public static boolean canWin(int leap, int[] game) {
-        
+public class Java1DArrayII 
+{
+    public static boolean canWin(int leap, int[] game) 
+    {
         int n = game.length;
         boolean used[] = new boolean[n];
         Stack<Integer> s = new Stack<>();
@@ -20,11 +20,7 @@ public class Java1DArrayII {
             int i = s.pop();
             if((i+1 >= n) || (i+leap >= n))
                 return true;
-            if((i-1 >= 0) && (game[i-1] == 0) && (used[i-1] == false))
-            {
-                s.push(i-1);
-                used[i-1] = true;
-            }
+            
             if((i+1 < n) && (game[i+1] == 0) && (used[i+1] == false))
             {
                 s.push(i+1);
@@ -35,9 +31,13 @@ public class Java1DArrayII {
                 s.push(i+leap);
                 used[i+leap] = true;
             }
+            if((i-1 >= 0) && (game[i-1] == 0) && (used[i-1] == false))
+            {
+                s.push(i-1);
+                used[i-1] = true;
+            }
         }
         return false;
-        
     }
 
     public static void main(String[] args) 
---
+++ Java/JavaStack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class JavaStack
+{	
+    public static boolean isBalanced(String input)
+    {
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+            if((c == '{') || (c == '[') || (c == '('))
+            {
+                s.push(c);
+            }
+            else if((c == '}') || (c == ']') || (c == ')'))
+            {
+                if(s.isEmpty())
+                {
+                    return (false);
+                }
+            }
+            else 
+            {
+                char last = s.pop();
+                if((c == '{' && last != '}') || (c == '[' && last != ']') || (c == '(' && last != ')'))
+                {
+                    return (false);
+                }
+            }
+        }
+        return (s.isEmpty());
+    }
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) 
+        {
+			String input=sc.next();
+            
+            System.out.println(isBalanced(input));
+		}
+        sc.close();		
+	}
+}
